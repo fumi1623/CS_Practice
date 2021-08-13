8821c0a5e826294e6e7afc77efa3cff125a614ce
fumi23 <78896035+fumi1623@users.noreply.github.com>
2021-08-13 13:59:04 +0900

    [start]

---
+++ Sample102/Program.cs~
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sample102 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write(123);
+            Console.WriteLine(456);
+
+            Console.Write("ABC");
+            Console.WriteLine("DEF");
+        }
+    }
+}
---
+++ Sample201/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Sample201 {
+    class Program {
+        static void Main(string[] args) {
+            //演算子
+            //Console.WriteLine("{0} + {1} = {2}", 5, 2, 5 + 2);
+            //Console.WriteLine("{0} - {1} = {2}", 5, 2, 5 - 2);
+            //Console.WriteLine("{0} * {1} = {2}", 5, 2, 5 * 2);
+            //Console.WriteLine("{0} / {1} = {2}　余り　{3}", 5, 2, 5 / 2, 5 % 2);
+
+            //変数
+            //int a;
+            //int b = 3;
+            //int add, sub;
+            //double avg;
+            //a = 6;
+            //add = a + b;
+            //sub = a - b;
+            //avg = (a + b) / 2.0;
+            //Console.WriteLine("{0} + {1} = {2}", a, b, add);
+            //Console.WriteLine("{0} - {1} = {2}", a, b, sub);
+            //Console.WriteLine("{0}と{1}の平均値{2}", a, b, avg);
+
+            //条件式
+            //Console.Write("整数値を入力");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("a=" + a);
+            //if (a > 0) {
+            //    Console.WriteLine("aは正の数");
+            //} else {
+            //    Console.WriteLine("aは負の数");
+            //}
+
+            Console.Write("1から3の整数を入力");
+            int num = int.Parse(Console.ReadLine());
+            switch (num) {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                default:
+                    Console.WriteLine("不適切な値です");
+                    break;
+            }
+        }
+    }
+}
