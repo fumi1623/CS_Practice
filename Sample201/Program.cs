cd41ecf7fc36d3ecb71204b8f18727da85d26d65
fumi23 <78896035+fumi1623@users.noreply.github.com>
2021-08-13 14:46:33 +0900

    [Add]sample3

--- Sample201/Program.cs
+++ Sample201/Program.cs
@@ -32,21 +32,23 @@ namespace Sample201 {
             //    Console.WriteLine("aは負の数");
             //}
 
-            Console.Write("1から3の整数を入力");
-            int num = int.Parse(Console.ReadLine());
-            switch (num) {
-                case 1:
-                    Console.WriteLine("one");
-                    break;
-                case 2:
-                    Console.WriteLine("two");
-                    break;
-                case 3:
-                    Console.WriteLine("three");
-                    break;
-                default:
-                    Console.WriteLine("不適切な値です");
-                    break;
+            //Console.Write("1から3の整数を入力");
+            //int num = int.Parse(Console.ReadLine());
+            //switch (num) {
+            //    case 1:
+            //        Console.WriteLine("one");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("two");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("three");
+            //        break;
+            //    default:
+            //        Console.WriteLine("不適切な値です");
+            //        break;
+
+
             }
         }
     }
---
+++ Sample301/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Sample301 {
+    class Program {
+        static void Main(string[] args) {
+            //繰り返し処理
+            //for (int i = 1; i <= 5; i++) {
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+            //Random rnd = new Random();
+            //Console.WriteLine("6が出たら終了");
+            //while (true) {
+            //    int dice = rnd.Next(1, 7);
+            //    Console.WriteLine(dice);
+            //    if(dice == 6) {
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine("終了");
+
+            //配列
+            //double[] d = new double[3];
+            //d[0] = 1.2;
+            //d[1] = 3.7;
+            //d[2] = 4.1;
+            //double sum, avg;
+            //sum = 0.0;
+            //for(int i = 0; i < d.Length; i++) {
+            //    Console.Write(d[i] + " ");
+            //    sum += d[i];
+            //}
+            //Console.WriteLine();
+            //avg = sum / d.Length;
+            //Console.WriteLine("合計値：" + sum);
+            //Console.WriteLine("平均値：" + avg);
+
+            int[,] a = new int[3, 4];
+            int m, n;
+            for (m = 0; m < 3; m++) {
+                for (n = 0; n < 4; n++) {
+                    a[m, n] = m + n;
+                }
+            }
+            for(m = 0; m < 3; m++) {
+                for(n = 0; n < 4; n++) {
+                    Console.Write("a[{0},{1}]={2}", m, n, a[m, n]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
